27a08e4e475aa3d3789c79ab9ec4888e8856dd8b
ronak4195 <4195rj@gmail.com>
2024-11-08 19:50:01 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,8 @@ app.use('/images', express.static('upload/images'));
 app.post("/upload", upload.single('product'), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`
+    // image_url: `http://localhost:${port}/images/${req.file.filename}`
+    image_url: `https://motomg.onrender.com/images/${req.file.filename}`
   });
 });
 
