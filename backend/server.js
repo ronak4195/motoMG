823cc3d0903e49c62ec63215fc6160357d278bc2
ronak4195 <4195rj@gmail.com>
2024-11-13 14:41:59 +0530

    cloudinary

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,53 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config(); 
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
 import multer from 'multer';
 import productRoutes from './routes/product.routes.js';
-import db from './config/db.config.js'
+import db from './config/db.config.js';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Enable CORS for all requests
-app.use(cors());
-//Middleware
-app.use(express.json());
-
-
-//Routes
-app.use('/', productRoutes);
-
-const storage = multer.diskStorage({
-  destination: './upload/images',
-  filename: (req, file, cb) => {
-    return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
-  }
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const upload = multer({ storage: storage });
+app.use(cors());
 
-app.use('/images', express.static('upload/images'));
-app.post("/upload", upload.single('product'), (req, res) => {
-  res.json({
-    success: 1,
-    // image_url: `http://localhost:${port}/images/${req.file.filename}`
-    image_url: `https://motomg.onrender.com/images/${req.file.filename}`
-  });
+app.use(express.json());
+
+app.use('/', productRoutes);
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'images', 
+    format: async (req, file) => 'png', 
+    public_id: (req, file) => `product_${Date.now()}`, 
+  },
+});
+
+const parser = multer({ storage: storage });
+
+app.post('/upload', parser.single('product'), (req, res) => {
+  if (req.file && req.file.path) {
+    res.json({
+      success: 1,
+      image_url: req.file.path, 
+    });
+  } else {
+    res.status(500).json({
+      success: 0,
+      message: 'File upload failed',
+    });
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
